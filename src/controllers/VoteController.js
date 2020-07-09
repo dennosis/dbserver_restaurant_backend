e578c9ba83f7bb19646d281c631be9a37830345f
dennosis <dennisaguiar@hotmail.com>
2020-07-09 03:49:42 -0300

    Correção para não escolher o mesmo restaurante

--- src/controllers/VoteController.js
+++ src/controllers/VoteController.js
@@ -1,5 +1,62 @@
 const Vote = require('../models/Vote');
 
+
+
+const validateRestaurantVote = async (vote) =>{
+    const week = await weekDays(await new Date(vote.date))
+
+    const winningRestaurantByDayFromWeekDays = week.map((date)=>winningRestaurantByDay(date))
+    
+    const checkRestaurant = winningRestaurantByDayFromWeekDays.filter((item)=>item.id===vote.restaurantId)
+
+    return (checkRestaurant.length === 0)
+}
+
+const winningRestaurantByDay = (findDate)=>{
+    const date = new Date(findDate)
+    const votes = Vote.find({"date":date.toLocaleDateString()})
+    const votesGroup = groupBy(votes, "restaurantId")
+    
+    const winning = {
+        id:undefined,
+        votes: []
+    }
+
+    Object.keys(votesGroup).forEach(function(key,index) {
+        if(votesGroup[key].length > winning.votes.length){
+            winning.id = key
+            winning.votes = votesGroup[key]
+        }
+    });
+
+    return winning
+}
+
+const groupBy = (xs, key) => {
+    return xs.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+};
+  
+const addDate = (initdate, days) =>{
+    const date = new Date(initdate)
+    date.setDate(date.getDate() + days);
+    return date;
+}
+const weekDays = async (initdate) =>{
+    const date = await new Date(initdate)
+    const weekday = await date.getDay()
+    const days = [date]
+    for(let i = weekday-1; i >=0; i-- ){
+        await days.unshift(addDate(date, (i-weekday)))
+    }
+    for(let i = weekday+1; i < 7; i++ ){
+        await days.push(addDate(date, (i-weekday)))
+    }
+    return days
+}
+
 module.exports = {
 
     index: async (req, res) => {
@@ -20,10 +77,16 @@ module.exports = {
             
             if((new Date(date)).getTime() < (new Date((new Date()).toLocaleDateString())).getTime())
                 return res.status(405).json({message: "Vote cannot be created on this date", error: {date}});
-
+             
             const vote = await new Vote({userId, restaurantId, date})
+            
+            if(!(await validateRestaurantVote(vote)))
+                return res.status(405).json({message: "This restaurant was already chosen in the week", error: {restaurantId}});
+
             await vote.save()
-            res.json(vote)
+            
+            return res.json(vote)
+
         } catch (error) {
             res.status(500).json({message: "Error create vote", error: error});
         }
